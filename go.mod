f0f86f465f7805bc56736d9a708c8efa6857d04b
Timur Demin <me@tdem.in>
2020-07-28 22:41:57 +0500

    Initial commit
    Add the first working version of syg_go.

---
+++ go.mod
@@ -0,0 +1,5 @@
+module git.tdem.in/tdemin/syg_go
+
+go 1.14
+
+require github.com/yggdrasil-network/yggdrasil-go v0.3.14
---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+/*
+Parts of this file are taken from yggdrasil-go, located at
+https://github.com/yggdrasil-network/yggdrasil-go
+
+See cmd/genkeys/main.go@78b5f88e4bb734d0dd6a138ff08d34ca39dcaea3
+*/
+
+import (
+	"encoding/hex"
+	"flag"
+	"log"
+	"net"
+	"os"
+	"regexp"
+	"runtime"
+
+	"github.com/yggdrasil-network/yggdrasil-go/src/address"
+	"github.com/yggdrasil-network/yggdrasil-go/src/crypto"
+)
+
+func main() {
+	rxflag := flag.String("regex", "::", "regex to match addresses against")
+	threads := flag.Int("threads", runtime.GOMAXPROCS(0), "how many threads to use for mining")
+	iterationsPerOutput := flag.Uint("iter", 100000, "per how many iterations to output status")
+	flag.Parse()
+	regex, err := regexp.Compile(*rxflag)
+	if err != nil {
+		log.Printf("%v\n", err)
+		os.Exit(1)
+	}
+
+	newKeys := make(chan keySet, *threads)
+	log.Printf("starting mining for %v with %v threads\n", regex, *threads)
+	for i := 0; i < *threads; i++ {
+		go doBoxKeys(newKeys)
+	}
+
+	counter := uint64(0)
+	i := uint64(*iterationsPerOutput)
+	for {
+		newKey := <-newKeys
+		if regex.MatchString(newKey.ip) {
+			log.Printf("priv: %s | pub: %s | nodeid: %s | ip: %s\n",
+				hex.EncodeToString(newKey.priv[:]),
+				hex.EncodeToString(newKey.pub[:]),
+				hex.EncodeToString(newKey.id[:]),
+				newKey.ip)
+		}
+		counter++
+		if counter%i == 0 {
+			log.Printf("reached %v iterations\n", counter)
+		}
+	}
+}
+
+type keySet struct {
+	priv []byte
+	pub  []byte
+	id   []byte
+	ip   string
+}
+
+func doBoxKeys(out chan<- keySet) {
+	for {
+		pub, priv := crypto.NewBoxKeys()
+		id := crypto.GetNodeID(pub)
+		ip := net.IP(address.AddrForNodeID(id)[:]).String()
+		out <- keySet{priv[:], pub[:], id[:], ip}
+	}
+}
